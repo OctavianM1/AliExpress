69059bd4c75713dbee29e0669ca0061a9534ef18
Octavian Mitu <octavian.mitu@amdaris.com>
2021-07-25 19:50:46 +0300

    http2

--- frontend/pages/Test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Test() {
-  return <div>HELOOOOOOOOOOO11111111111111111111111999999999999999999999999999999999999999999999</div>;
-}
-
-export default Test;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Test from './Test';
+import Test from '../components/Test';
 
 export default function Home() {
   return (
