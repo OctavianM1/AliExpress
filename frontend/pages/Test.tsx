3696d9167e5fbd6d96a17a776ec0b24be510cd97
Octavian Mitu <octavian.mitu@amdaris.com>
2021-07-25 19:26:14 +0300

    modify test

--- frontend/pages/Test.tsx
+++ frontend/pages/Test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Test() {
-  return <div>HELOOOOOOOOOOO11111111111111111111111</div>;
+  return <div>HELOOOOOOOOOOO11111111111111111111111999999999999999999999999999999999999999999999</div>;
 }
 
 export default Test;
